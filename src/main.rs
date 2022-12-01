7a2de0ee97de99bbb24d96432fd31d59b4964af0
torijacarlos <hi@torijacarlos.com>
2022-11-30 19:45:02 -0600

    feature: basic belvo-rust sdk usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use belvo::client::{BelvoClient, BelvoKey, Environment};
+use belvo::link::{api as link_api, AccessMode, LinkBase, LinkDetail, LinkFilters, LinkStatus};
+use serde::Deserialize;
+use std::error::Error;
+use std::fs::File;
+use std::io::{Error as IoError, Read};
+
+#[derive(Deserialize)]
+struct Config {
+    belvo: BelvoKey,
+    institution: LinkBase,
+}
+
+impl Config {
+    /// Attemps to load a config file from the `file_path` provided
+    fn from_file(file_path: String) -> Result<Config, IoError> {
+        let mut config_file = File::open(file_path).expect("Config file not found");
+        let mut contents: String = String::from("");
+
+        config_file.read_to_string(&mut contents)?;
+        let config: Config = toml::from_str(&contents)?;
+
+        Ok(config)
+    }
+}
+
+async fn get_valid_link() -> Option<LinkDetail> {
+    let file_path = format!("./config.{}.toml", Environment::Development);
+    let config = Config::from_file(file_path).unwrap();
+    let belvo_client = BelvoClient::new(config.belvo.to_owned(), Environment::Development);
+    let filters = LinkFilters {
+        access_mode: Some(AccessMode::Single),
+        status: Some(LinkStatus::Valid),
+    };
+    if let Ok(results) = link_api::list(&filters, &belvo_client).await {
+        if results.count != 0 {
+            return results.results.to_vec().pop();
+        }
+    }
+    if let Ok(link) = link_api::register(&config.institution, &belvo_client).await {
+        return Some(link);
+    }
+
+    return None;
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let link: Option<LinkDetail> = get_valid_link().await;
+
+    if let Some(l) = link {
+        println!("{:?}", l);
+    } else {
+        println!("No link found or created");
+    }
+
+    Ok(())
 }
